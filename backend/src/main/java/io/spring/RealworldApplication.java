9e9f3c0edd1ba8c71ea4a82bc7c4357e2871a1e3
pavithra Atrangada <pavithraatrangada@pavithras-iMac.local>
2021-03-14 18:13:04 -0500

    first check in

--- backend/src/main/java/io/spring/RealworldApplication.java
+++ backend/src/main/java/io/spring/RealworldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealworldApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Real World");
 		SpringApplication.run(RealworldApplication.class, args);
 	}
 }
